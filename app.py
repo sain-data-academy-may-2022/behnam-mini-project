112154d484f7def8ecb47f7f05d3c1f51fbda5f7
Behnam Vakili <behnamkvl@gmail.com>
2022-07-18 13:51:02 +0100

    TEST ecr login

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from utils.db_functions import initialize_db
-from utils.main_menu import run_main_menu
+# from utils.db_functions import initialize_db
+# from utils.main_menu import run_main_menu
 
 
-DB_FILE = "./db/caffe.sql"
+# DB_FILE = "./db/caffe.sql"
 
 
-if __name__ == "__main__":
-    initialize_db(DB_FILE)
-    run_main_menu()
+# if __name__ == "__main__":
+#     initialize_db(DB_FILE)
+#     run_main_menu()
+
+
+def handler(*args, **kargs):
+    return "hello!"
